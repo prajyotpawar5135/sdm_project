1a2ea06f8baf76cf5f08e50f338e994be56516b7
RoshanDahaghane <131250531+RoshanDahaghane@users.noreply.github.com>
2023-04-19 21:03:20 +0530

    Add files via upload

---
+++ Inputdata.sql
@@ -0,0 +1,31 @@
+DATA ENTRY FOR THEATER
+
+insert into theater values(1001,'PVR Cinemas','Pimpri');
+insert into theater values(1002,'Ashok Cinema','Power house road');
+insert into theater values(1003,'INOX Cinemas','Chinchwad Gao');
+insert into theater values(1004,'Carnival Cinemas','Chinchwad East');
+insert into theater values(1005,'CITY Pride Cinema','Rahatani');
+
+DATA ENTRY FOR CUSTOMER
+
+insert into customer values(1,'Michal Rao',10,'Munna Michal');
+insert into customer values(2,'Prajyot Bassi',11,'Pushpa');
+insert into customer values(3,'Pawan Dhamale',12,'Krish');
+insert into customer values(4,'Faran Chaudhary',13,'Fhitoor');
+insert into customer values(5,'Sarang chopde',14,'Makkhi');
+
+DATA ENTRY FOR TICKET
+
+insert into ticket values(10001,'Munna Michal','2hr',5,350);
+insert into ticket values(10002,'Makkhi','2hr 25min',6,400);
+insert into ticket values(10003,'Krish','1hr 15min',2,100);
+insert into ticket values(10004,'Fhitoor','2hr 45min',4,300);
+insert into ticket values(10005,'Pushpa','3hr',8,500);
+
+DATA ENTRY FOR MOVIE
+
+insert into movie values(100001,'Munna Michal','Morning-9:00',400001,1001);
+insert into movie values(100001,'Makkhi','Afternoon-18:00',400002,1002);
+insert into movie values(100001,'Krish','Afternoon-14:00',400003,1003);
+insert into movie values(100001,'Fhitoor','Morning-9:00',400004,1004);
+insert into movie values(100001,'Pushpa','Night-23:00',400005,1005);
